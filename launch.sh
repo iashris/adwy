8705d8e2297ea38c79871dc3d478ef38a39916fe
Ashris Choudhury <ashris.me@gmail.com>
2018-05-29 11:34:31 +0530

    no clue

--- launch.sh
+++ launch.sh
@@ -1,24 +1,22 @@
-echo ------ Installing required packages ------
-pip install beautifulsoup4
-echo ------ ${bold}Done ✓${normal} ------
+#If you are on Windows and cannot run launch.sh, follow the comments
+#Ignore all the lines that start with echo
+
 echo ------ Initializing message parsing ------
 echo ------ This process can take upto 5 minutes ------
+#Run the parser.py with the following command
 python parser.py
 echo ------ ${bold}Awesome, Done ✓${normal}  ------
 echo ------ Launching Website ------
-
-
+#open localhost:8000 now, it wont work however until you launch the http server
 open http://localhost:8000/
-
 # get python version
-
 PYV=`python -c "import sys;t='{v[0]}'.format(v=list(sys.version_info[:2]));sys.stdout.write(t)";`
 if (( $PYV == 2 ))
 then
-    python -m SimpleHTTPServer #Python 2
+    python -m SimpleHTTPServer # just run this for Python 2
 elif (( $PYV == 3 ))
 then
-    python -m http.server #Python 3
+    python -m http.server #run this for Python 3
 else
     echo YOU MUST INSTALL PYTHON 2 OR 3
 fi
